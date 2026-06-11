3f29a86841c1b0011c8662f3e4617ae3773e0371
Bryan Mancilla <bmancilla119@gmail.com>
2026-06-11 14:57:30 -0500

    feat: implementar user repository en login

--- app/src/main/java/com/bryan/taskflow/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/bryan/taskflow/presentation/login/LoginViewModel.kt
@@ -1,16 +1,21 @@
 package com.bryan.taskflow.presentation.login
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bryan.taskflow.domain.repository.UserRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 /**
  * Maneja la lógica de presentación del login.
  *
  * La UI observa uiState mediante StateFlow  y se recompone automáticamente cuando el estado cambia
  */
-class LoginViewModel: ViewModel(){
+class LoginViewModel(
+    private val userRepository: UserRepository
+): ViewModel(){
     private val _uiState = MutableStateFlow(LoginUiState())
     val uiState: StateFlow<LoginUiState> = _uiState.asStateFlow()
 
@@ -23,19 +28,38 @@ class LoginViewModel: ViewModel(){
     }
 
     fun login(){
+        val state = _uiState.value
         if (
-           _uiState.value.email.isBlank() ||
-            _uiState.value.password.isBlank()
+           state.email.isBlank() ||
+            state.password.isBlank()
         ){
-            _uiState.value = _uiState.value.copy(
+            _uiState.value = state.copy(
                 error = "Complete todos los campos"
             )
 
             return
         }
-        _uiState.value = _uiState.value.copy(
-            error = null,
-            isLoggedIn = true
-        )
+//        _uiState.value = _uiState.value.copy(
+//            error = null,
+//            isLoggedIn = true
+//        )
+        viewModelScope.launch {
+            val result = userRepository.login(
+                email = state.email,
+                password = state.password
+            )
+            result
+                .onSuccess {
+                    _uiState.value = state.copy(
+                        error = null,
+                        isLoggedIn = true
+                    )
+                }
+                .onFailure {
+                    _uiState.value = state.copy(
+                        error = it.message
+                    )
+                }
+        }
     }
 }
