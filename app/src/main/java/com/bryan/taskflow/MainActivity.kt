71291ad7044b928374588bd7007669c822d8609e
Bryan Mancilla <bmancilla119@gmail.com>
2026-06-11 15:03:52 -0500

    feat: inyectar el repositorio de usuario en los view models

--- app/src/main/java/com/bryan/taskflow/MainActivity.kt
+++ app/src/main/java/com/bryan/taskflow/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.*
+import com.bryan.taskflow.data.repository.RepositoryProvider
 import com.bryan.taskflow.presentation.login.LoginViewModel
+import com.bryan.taskflow.presentation.login.RegisterViewModel
 import com.bryan.taskflow.presentation.task.TaskViewModel
 import com.bryan.taskflow.ui.screens.HomeScreen
 import com.bryan.taskflow.ui.screens.LoginScreen
+import com.bryan.taskflow.ui.screens.RegisterScreen
 import com.bryan.taskflow.ui.screens.TaskScreen
 import com.bryan.taskflow.ui.theme.TaskFlowTheme
 
@@ -18,15 +21,41 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TaskFlowTheme {
+               val userRepository = remember {
+                   RepositoryProvider.provideUserRepository(this@MainActivity)
+               }
+
+                val loginViewModel = remember {
+                    LoginViewModel(userRepository)
+                }
+
+                val registerViewModel = remember {
+                    RegisterViewModel(userRepository)
+                }
+
                 var currentScreen by remember {
                     mutableStateOf("login")
                 }
 
                 when(currentScreen){
                     "login" -> LoginScreen(
-                        viewModel = LoginViewModel(),
+//                        viewModel = LoginViewModel(),
+                        viewModel = loginViewModel,
                         onLoginSuccess = {
                             currentScreen = "home"
+                        },
+                        onNavigationToRegister = {
+                            currentScreen = "register"
+                        }
+                    )
+                    "register"-> RegisterScreen(
+//                        viewModel = RegisterViewModel(),
+                        viewModel = registerViewModel,
+                        onRegisterSuccess = {
+                            currentScreen = "login"
+                        },
+                        onNavigationToLogin = {
+                            currentScreen = "login"
                         }
                     )
 //                    "home" -> HomeScreen()
